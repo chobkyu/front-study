d7875027ea1dcbfd01cac41cf7e1f64fdafe4521
bk0625 <qudqud97@amicuslex.net>
2024-09-02 11:10:10 +0900

    setting

--- app/atom.ts
+++ app/atom.ts
@@ -4,12 +4,3 @@ export const todoListState = atom({
     key: "todoListState",
     default: []
 })
-/**
- * echo "# front-study" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/chobkyu/front-study.git
-git push -u origin main
- */
